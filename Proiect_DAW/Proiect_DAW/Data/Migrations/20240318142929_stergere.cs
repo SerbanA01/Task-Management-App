a1c6147607eae2686accd2bb5e986642e4aa3760
SerbanA01 <andrei.serban2@s.unibuc.ro>
2024-03-18 16:32:09 +0200

    Task-management

---
+++ Proiect_DAW/Proiect_DAW/Data/Migrations/20240318142929_stergere.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Proiect_DAW.Data.Migrations
+{
+    public partial class stergere : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
